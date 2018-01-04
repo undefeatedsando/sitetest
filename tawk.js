2733e0de3bcb8043c2775306aef70a440de19848
undefeatedsando <thekaidaten@gmail.com>
2018-01-04 16:55:20 +0300

    Test of tawk 2

---
+++ tawk.js
@@ -0,0 +1,11 @@
+//Start of Tawk.to Script
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5a4df77fd7591465c7067ea2/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+//End of Tawk.to Script
