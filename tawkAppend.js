7f244b891ce7e8a14ec08df8223161b1e44d4895
undefeatedsando <thekaidaten@gmail.com>
2018-01-04 18:31:48 +0300

    Try to change chat

--- tawkAppend.js
+++ tawkAppend.js
@@ -1,5 +1,5 @@
 function changeChatState (chatID = 0){
     var frames = document.querySelectorAll("iframe");
     frames[0].style.display = "block";
-    frames[1].style.display = "none";
+    frames[1].style.display = "block";
 }
