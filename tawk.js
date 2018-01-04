6c6b49774e8c4fd372a80f89fe89034cb3866320
undefeatedsando <thekaidaten@gmail.com>
2018-01-04 17:15:13 +0300

    Test of iframe selector

--- tawk.js
+++ tawk.js
@@ -2,7 +2,7 @@
 var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
+s1.async=false;
 s1.src='https://embed.tawk.to/5a4df77fd7591465c7067ea2/default';
 s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
