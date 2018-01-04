242f91a64becb406140705c2f8a35b2e7e757a04
undefeatedsando <thekaidaten@gmail.com>
2018-01-04 18:51:38 +0300

    Update tawkAppend.js

--- tawkAppend.js
+++ tawkAppend.js
@@ -2,7 +2,7 @@ function changeChatState (chatID = 0){
     var frames = document.querySelectorAll("iframe");
     frames[0].style.display = "block";
     frames[1].style.display = "none";
-    console.log("What the hell");
+    //console.log("What the hell");
 }
 
 setTimeout(changeChatState, 1000);
