b23e7d1e8d1f399958c8963be5ea0cf5516bee9a
undefeatedsando <thekaidaten@gmail.com>
2018-01-04 20:51:11 +0300

    Embed

--- tawkAppend.js
+++ tawkAppend.js
@@ -4,4 +4,20 @@ function changeChatState (chatID = 0){
     frames[1].style.display = "none";
 }
 
-setTimeout(changeChatState, 10);
+function appendClose(){
+    var closebutton = document.getElementById("endChat");
+    closebutton.addEventListener("click", closeChat, false);
+    console.log(closebutton);
+}
+
+function closeChat(){
+    var frames = document.querySelectorAll("iframe");
+    frames[0].style.display = "none";
+    frames[1].style.display = "none";
+}
+
+setTimeout(appendClose, 500);
+appendClose();
+appendClose();
+
+setTimeout(changeChatState, 500);
