fe368b27fe1536ac4ae4b1e9d42d806222ef82e0
undefeatedsando <thekaidaten@gmail.com>
2018-04-27 18:27:49 +0300

    Chage stuff from default
    Why it still worked on?

--- tawk.js
+++ tawk.js
@@ -1,13 +1,8 @@
-//Start of Tawk.to Script
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date(); Tawk_API.embedded='tawk_5a4df77fd7591465c7067ea2';
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=false;
-s1.src='https://embed.tawk.to/5a4df77fd7591465c7067ea2/default';
+s1.async=true;
+s1.src='https://embed.tawk.to/5a4df77fd7591465c7067ea2/1c314ma4l';
 s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-    
-})();
-//End of Tawk.to Script
-
+s0.parentNode.insertBefore(s1,s0);})();
